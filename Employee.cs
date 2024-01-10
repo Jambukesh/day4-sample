fb0bd9dfc36168ecddb858cd0586bd57e4b228e0
Jambukesh <jkesh360@gmail.com>
2024-01-10 09:46:42 +0200

    day4

---
+++ Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string? Designation { get; set; }
+        public DateTime Doj { get; set; }
+
+
+    }
+}
